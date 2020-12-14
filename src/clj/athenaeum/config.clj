757be75d8dffae5599fa61b040ec0b6ea507368d
Akshatha Herle <akshatha@nilenso.com>
2020-12-14 16:18:19 +0530

    Use aero to manage config

--- src/clj/athenaeum/config.clj
+++ src/clj/athenaeum/config.clj
@@ -1,8 +1,10 @@
 (ns athenaeum.config
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.java.io :as io]
+            [aero.core :as aero]))
 
 (defonce config (atom nil))
 
 (defn load-config
   ([] (load-config "config.edn"))
-  ([file-name] (reset! config (edn/read-string (slurp file-name)))))
+  ([file-name] (reset! config (aero/read-config
+                               (io/resource file-name)))))
