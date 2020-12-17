6212dcd86feb7d144eae0959461c0057596b252f
Akshatha Herle <akshatha@nilenso.com>
2020-12-17 22:29:26 +0530

    Add handler tests and fixtures

--- test/clj/athenaeum/books/handlers_test.clj
+++ test/clj/athenaeum/books/handlers_test.clj
@@ -1,2 +1,23 @@
 (ns athenaeum.books.handlers-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [athenaeum.books.handlers :as books]
+            [athenaeum.fixtures :as f]
+            [athenaeum.db :as db]))
+
+(use-fixtures :once f/config-and-datasource)
+(use-fixtures :each f/clear-tables)
+
+(deftest fetch-test
+  (testing "Returns string of books"
+    (let [res (books/fetch @db/datasource)]
+      (is (= 200 (:status res)))
+      (is (= "[]" (:body res))))))
+
+(deftest create-test
+  (testing "Adds book given its name and author"
+    (let [req {:params {:name   "test-book"
+                        :author "test-author"}}
+          res (books/create req)]
+      (is (= 200 (:status res)))
+      (is (= "#:books{:id 2, :name \"test-book\", :author \"test-author\", :isbn nil, :year_of_publication nil}"
+             (:body res))))))
--- test/clj/athenaeum/fixtures.clj
+++ test/clj/athenaeum/fixtures.clj
@@ -1,2 +1,17 @@
 (ns athenaeum.fixtures
-  (:require [clojure.test :refer :all]))
+  (:require [next.jdbc :as jdbc]
+            [clojure.edn :as edn]
+            [athenaeum.db :as db]))
+
+(defonce test-config (atom nil))
+
+(defn config-and-datasource
+  [f]
+  (reset! test-config (edn/read-string (slurp "config/config.test.edn")))
+  (db/set-datasource test-config)
+  (f))
+
+(defn clear-tables
+  [f]
+  (jdbc/execute! @db/datasource ["TRUNCATE TABLE books CASCADE"])
+  (f))
