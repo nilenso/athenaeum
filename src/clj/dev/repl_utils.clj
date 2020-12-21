b06440200d4830781a63986a6bf7245fd927ad72
Akshatha Herle <akshatha@nilenso.com>
2020-12-21 11:00:24 +0530

    Install postgresql 12 on staging

--- src/clj/dev/repl_utils.clj
+++ src/clj/dev/repl_utils.clj
@@ -2,12 +2,14 @@
   (:require [athenaeum.server :as server]
             [athenaeum.config :as config]
             [athenaeum.db :as db]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [athenaeum.migrations :as migrations]))
 
 (defn start-app
   []
   (config/load-config "config/config.dev.edn")
   (db/set-datasource)
+  (migrations/migrate)
   (reset! server/server (jetty/run-jetty #'server/handler
                                          {:host  "localhost"
                                           :port  (:ring-server-port @config/config)
