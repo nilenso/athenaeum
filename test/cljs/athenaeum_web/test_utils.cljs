484100b56193ec0f4220dc2601e73fdabbcbed1c
Akshatha Herle <akshatha@nilenso.com>
2020-12-24 17:39:26 +0530

    Write event handler test with api call stubbing

--- test/cljs/athenaeum_web/test_utils.cljs
+++ test/cljs/athenaeum_web/test_utils.cljs
@@ -1,1 +1,11 @@
-(ns athenaeum-web.test-utils)
+(ns athenaeum-web.test-utils
+  (:require [re-frame.core :as rf]))
+
+(defn stub-api-call
+  [response success?]
+  (rf/reg-fx
+   :http-xhrio
+   (fn [{:keys [on-success on-failure]}]
+     (if success?
+       (rf/dispatch (conj on-success response))
+       (rf/dispatch (conj on-failure response))))))
